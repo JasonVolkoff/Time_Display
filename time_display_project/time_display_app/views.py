1f9bfbb13535e646368ddf970b97d98669586382
JasonVolkoff <kashijason@gmail.com>
2021-04-18 03:33:39 -0700

    Project state: submission

--- time_display_project/time_display_app/views.py
+++ time_display_project/time_display_app/views.py
@@ -4,7 +4,7 @@ from time import gmtime, strftime
 
 def index(request):
     context = {
-        "date": strftime("%Y-%m-%d", gmtime()),
+        "date": strftime("%b %d, %Y", gmtime()),
         "time": strftime("%H:%M %p", gmtime()),
     }
     return render(request, "index.html", context)
